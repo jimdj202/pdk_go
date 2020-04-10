b2149881f3ab5e8a56ad0e4295eeb602dbbd29e0
abc <abc@qq.com>
2020-04-10 15:24:14 +0800

    提交备份

--- src/server/model/qin_you_quan_data.go
+++ src/server/model/qin_you_quan_data.go
@@ -3,12 +3,14 @@ package model
 //亲友圈基本信息
 import (
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
 	"pdk/src/server/lib/db"
 	"time"
 )
 
 type QinYouQuan struct {
 	ID        uint32 `gorm:"primary_key;AUTO_INCREMENT;index;unique_index;type:BIGINT"`
+	Qid 	  uint32 `gorm:"primary_key;index;type:MEDIUMINT"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
@@ -22,9 +24,29 @@ func (q *QinYouQuan)Create()(int64,error)  {
 	return dbResult.RowsAffected,dbResult.Error
 }
 
-func (q *QinYouQuan) Delete()(int64,error) {
-	dbResult := db.GetGormDB().Delete(q)
-	return dbResult.RowsAffected,dbResult.Error
+func (q *QinYouQuan) Delete() error {
+	return db.GetGormDB().Transaction(func(tx *gorm.DB) error {
+		// 在事务中做一些数据库操作 (这里应该使用 'tx' ，而不是 'db')
+		dbResult := tx.Where("qid = ? ",q.Qid).Delete(QinYouQuanMember{})
+		if err := dbResult.Error;err != nil {
+			return err
+		}
+		dbResult2 := tx.Delete(q)
+		if err := dbResult2.Error;err != nil {
+			return err
+		}
+		//if err := tx.Delete(&Animal{Name: "Giraffe"}).Error; err != nil {
+		//	// 返回任意 err ，整个事务都会 rollback
+		//	return err
+		//}
+		//
+		//if err := tx.Create(&Animal{Name: "Lion"}).Error; err != nil {
+		//	return err
+		//}
+
+		// 返回 nil 提交事务
+		return nil
+	})
 }
 
 
--- src/server/model/qin_you_quan_member_data.go
+++ src/server/model/qin_you_quan_member_data.go
@@ -1,6 +1,9 @@
 package model
 
-import "time"
+import (
+	"pdk/src/server/lib/db"
+	"time"
+)
 
 type QinYouQuanMember struct {
 	ID        uint32 `gorm:"primary_key;AUTO_INCREMENT;type:BIGINT"`
@@ -8,5 +11,22 @@ type QinYouQuanMember struct {
 	Qid		uint32 `gorm:"primary_key;index;type:MEDIUMINT"`
 	Uid		uint32 `gorm:"primary_key;index;type:BIGINT"`
 	Qname   string `gorm:"type:VARCHAR(16)"`
+	Uname	string `gorm:"type:VARCHAR(16)"`
+	Status  uint8 `gorm:"type:SMALLINT"`
+
 }
 
+func (q *QinYouQuanMember) Create () (int64,error){
+	dbResult := db.GetGormDB().Create(q)
+	return dbResult.RowsAffected,dbResult.Error
+}
+
+func (q *QinYouQuanMember) Delete() (int64,error) {
+	dbResult := db.GetGormDB().Delete(q)
+	return dbResult.RowsAffected,dbResult.Error
+}
+
+func (q *QinYouQuanMember) DeleteByQid() (int64,error) {
+	dbResult := db.GetGormDB().Where("qid = ? AND uid = ? ",q.Qid,q.Uid).Delete(QinYouQuanMember{})
+	return dbResult.RowsAffected,dbResult.Error
+}
